648ce46886ad9b0409160fbd3cb32cb2bed0c2bc
ubuntu <ubuntu@localhost.localdomain>
2022-03-23 00:17:30 +0800

    first commit

---
+++ packages/uranus/src/app/app.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const StyledApp = styled.div`
+  // Your style here
+`;
+
+export function App() {
+  return (
+    <StyledApp>
+      <h2>dfgh</h2>
+    </StyledApp>
+  );
+}
+
+export default App;
