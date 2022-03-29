b7c49fa9c1664c9e3ef8192f8788ff9148a85bb6
panh1992 <panh1992@163.com>
2022-03-29 10:16:15 +0800

    upgrade nx

--- packages/uranus/src/main.tsx
+++ packages/uranus/src/main.tsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { CssBaseline } from '@mui/material';
 
 import App from './app/app';
 
 ReactDOM.render(
   <StrictMode>
+    <CssBaseline />
     <BrowserRouter>
       <App />
     </BrowserRouter>
